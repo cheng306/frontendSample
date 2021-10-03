0970472799d7e95724d8494808400e902753eb36
cheng306 <johnnycheng0306@gmail.com>
2021-10-03 11:03:46 -0400

    10-2-2021

--- src/exportModule.js
+++ src/exportModule.js
@@ -1,3 +1,7 @@
+console.log("exportModule:");
+console.log(import.meta);
+console.log(this);
+
 export function exportFunction(input){
   return `exprtFunction accept ${input}`;
 }
@@ -10,11 +14,15 @@ export function exportFunction3(input){
   return `exprtFunction accept ${input}`;
 }
 
-console.log("exportModule:");
-console.log(import.meta);
-console.log(this);
+
+export let prom1 = new Promise(function(res, rej){
+    setTimeout(function(){rej(12)}, 2000);
+});
 
 
+prom1.catch(function(e){
+    console.log(e);
+});
 // document.addEventListener("DOMContentLoaded", function(event){
 //   console.log("DOMContentLoaded");
 //   console.log(event);
--- src/importModule.js
+++ src/importModule.js
@@ -1,11 +1,16 @@
-// import {exportFunction, exportFunction2, exportFunction3} from './exportModule.js';
+import {exportFunction, exportFunction2, exportFunction3, prom1} from './exportModule.js';
 
-import {obj1} from"./exportModule2.js";
+// import {obj1} from"./exportModule2.js";
 
 
 console.log("importModule:");
 console.log(import.meta);
 console.log(this);
+
+// prom1.catch(function(e){
+//     console.log(e);
+// });
+
 
 
 
